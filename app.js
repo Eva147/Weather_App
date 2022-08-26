c556c66b864bb719427426ea067dcb34c6c1029b
Evgeniya <eva147@list.ru>
2022-08-26 12:40:53 +0100

    Change app.port

--- app.js
+++ app.js
@@ -113,10 +113,8 @@ function timeConverter(time, offset) {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+  port = 5000;
 }
-app.listen(port);
-
 app.listen(port, function() {
-    console.log('Server is running on port 3000.')
+    console.log(`Server is running on port ${port}.`)
 })
